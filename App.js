630f0f3a4b6ff45f1d5ecdd715d0795deb82182c
kimyoosang <yoo99485@gmail.com>
2022-01-31 19:33:51 +0900

    base

---
+++ App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import PageContainer from "./Pages/PageContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <PageContainer />
+    </div>
+  );
+}
+
+export default App;
