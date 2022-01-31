1d8369bab7fcfa29dbb8b705f065316ea2f2c8f5
kimyoosang <yoo99485@gmail.com>
2022-01-31 20:07:10 +0900

    [Add] Footer component

---
+++ src/Pages/Footer/Footer.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <section className="section-4">
+      <div className="section-4-text">
+        <h2 className="section-4-heading">Sign Me Up</h2>
+        <p className="section-4-paragraph">
+          Be the first to know about new products
+        </p>
+      </div>
+      <form className="signyo-form">
+        <input
+          type="email"
+          className="signup-form-input"
+          placeholder="Enter Your Email"
+        />
+        <button className="signup-form-btn" type="submit">
+          <i className="fas fa-arrow-right"></i>
+        </button>
+      </form>
+      <p className="copyright">CodeAndCreate &copy;All Right Reserved</p>
+    </section>
+  );
+}
+
+export default Footer;
